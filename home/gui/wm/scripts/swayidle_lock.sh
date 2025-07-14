c6139ab9dcac1ee7759e586c859048dd514fad3c
Patrick Ferris <patrick@sirref.org>
2025-07-14 12:47:06 +0100

    sway lock

--- home/gui/wm/scripts/swayidle_lock.sh
+++ home/gui/wm/scripts/swayidle_lock.sh
@@ -4,9 +4,9 @@ pkill swayidle
 
 swayidle -w\
 	lock '@locker@'\
-	timeout 120 "notify-send 'going to sleep soon!' -t 3000"\
-	timeout 180 '@wmmsg@ "output * dpms off"'\
+	timeout 300 "notify-send 'going to sleep soon!' -t 3000"\
+	timeout 360 '@wmmsg@ "output * dpms off"'\
 		resume '@wmmsg@ "output * dpms on"'\
-	timeout 240 'loginctl lock-session'\
+	timeout 420 'loginctl lock-session'\
 	before-sleep 'playerctl -a pause; loginctl lock-session'
 
